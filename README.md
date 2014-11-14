a8cb4fe0d628f404a25e1cabc4aa726027f4edfa
archcra@qq.com <archcra@qq.com>
2014-11-14 10:43:39 +0800

    Change images folder from 'images' to 'photo' to protect privacy

--- README.md
+++ README.md
@@ -4,12 +4,11 @@ PhotoGalleryDemo01
 像册演示一-HTML5
 
 这是一个像册应用，用于演示照片显示的一些效果。供照片演示使用。
-
-演示效果有：
+主体效果采用Impress.js ，其它还有一些演示效果如：
 星球大战字幕效果
 纵向滚屏效果(horizontal marquee)
 横向滚屏效果(vertical marquee)
 
 
-实现上主要使用HTML5来做展示，后台使用node.js。
+实现上主要使用HTML5来做展示，后台使用node.js做服务。
 
--- public/intermission/scripts/images.js
+++ public/intermission/scripts/images.js
@@ -1,73 +1,36 @@
-
-
 function padDigits(number, digits) {
-	return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
 }
 //Test
 //console.log(padDigits(1,2));
 
-function initImageObjects(){
-	var imageObjects = [];
-	for (i=0;i<54;i++){
-		var f= "images/set0/" + padDigits(i,2) + ".jpg";
-		imageObjects.push({"id":i, "file":f});
-	}
+function initImageObjects() {
+    var imageObjects = [];
+    for (i = 0; i < 54; i++) {
+        var f = "photo/set0/" + padDigits(i, 2) + ".jpg";
+        imageObjects.push({
+            "id": i,
+            "file": f
+        });
+    }
 
-	return imageObjects;
+    return imageObjects;
 }
 
-function initImages(){
-	var imageObjects = initImageObjects();
-	var imagesElement = document.getElementById('photos');
-		console.log('html0',imagesElement.innerHTML);
-
-	imagesElement.innerHTML = '';
-	console.log('html',imagesElement.innerHTML);
-	var newImage;
-	for (i=0;i<imageObjects.length;i++){
-		newImage = document.createElement('img');
-		newImage.setAttribute('id', imageObjects[i].id); 
-		newImage.setAttribute('alt', imageObjects[i].id); 
-	 	newImage.setAttribute('src', imageObjects[i].file);//'./images/common/phoneHLeft.png'); 
-newImage.setAttribute('style', 'display:none'); 
-imagesElement.appendChild(newImage);
-}
-}
-
-function changePhotos(){
-	console.log("change photos")
-  var imageObjects = [];
-	for (i=0;i<54;i++){
-		//var f= "images/set2/" + padDigits(i,2) + ".png";
-		var f= "images/set2/" +  "0.jpeg";
-		imageObjects.push({"id":i, "file":f});
-	}
-
-
+function initImages() {
+    var imageObjects = initImageObjects();
     var imagesElement = document.getElementById('photos');
-    //remove the dom object.
-   // imagesElement.parentNode.removeChild(frameid);
-   //var list=document.getElementById("myList");
-   // list.removeChild(list.childNodes[0]);
-   /*
-    var count = imagesElement.childNodes.length;
+    console.log('html0', imagesElement.innerHTML);
 
-    for(i=count; i>0; i--){
-       imagesElement.removeChild(imagesElement.childNodes[i-1]);
-
+    imagesElement.innerHTML = '';
+    console.log('html', imagesElement.innerHTML);
+    var newImage;
+    for (i = 0; i < imageObjects.length; i++) {
+        newImage = document.createElement('img');
+        newImage.setAttribute('id', imageObjects[i].id);
+        newImage.setAttribute('alt', imageObjects[i].id);
+        newImage.setAttribute('src', imageObjects[i].file);
+        newImage.setAttribute('style', 'display:none');
+        imagesElement.appendChild(newImage);
     }
-  */
-    //var newImage;
-	for (i=0;i<imageObjects.length;i++){
-		//newImage = document.createElement('img');
-		//newImage.setAttribute('id', imageObjects[i].id); 
-		//newImage.setAttribute('alt', imageObjects[i].id); 
-	 	//newImage.setAttribute('src', imageObjects[i].file);//'./images/common/phoneHLeft.png'); 
-     //newImage.setAttribute('style', 'display:none'); 
-     imagesElement.childNodes[i]["src"] =  imageObjects[i].file;
-    }
-
-   console.log("change photos end.")
-
-   return imageObjects;
 }
